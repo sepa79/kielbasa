0204f02cf2dd13049e87db94706d9e6e89a1ff40
SEPA <sepa79@gmail.com>
2023-08-13 00:05:00 +0200

    dog anim

--- menu/shopOutside.c
+++ menu/shopOutside.c
@@ -15,14 +15,28 @@
 #define SHOP_OPEN_WEEK_O2 15
 #define SHOP_OPEN_WEEK_C2 19
 
+#define SHOP_OUTSIDE_ANIM_FRAMES 12
+#define SHOP_OUTSIDE_ANIM_X 24+100
+#define SHOP_OUTSIDE_ANIM_Y 50+50
+#define SHOP_OUTSIDE_ANIM_C1 1
+#define SHOP_OUTSIDE_ANIM_C2 1
+#define SHOP_OUTSIDE_SMC1 9
+#define SHOP_OUTSIDE_SMC2 8
+#define SHOP_OUTSIDE_SPR_BANK 0xbc
+#define SHOP_OUTSIDE_SPR_BANK_ANIM 0xbd
+
+volatile char animFrame = 0;
+volatile char animDelay = 1;
+
 // Sections and regions
 #pragma section( shopOutLoaderData, 0 )
 #pragma section( shopOutCode, 0 )
+#pragma section( shopOutData, 0 )
 #pragma section( shopOutGfxDay, 0 )
 #pragma section( shopOutGfxNight, 0 )
-#pragma region( bankShopOutC, 0x8000, 0x9000, , MENU_BANK_SHOP, { shopOutLoaderData, shopOutCode } )
+#pragma region( bankShopOutC, 0x8000, 0x9000, , MENU_BANK_SHOP, { shopOutLoaderData, shopOutCode, shopOutData } )
 #pragma region( bankShopOutG1, DAY_GFX_BMP, DAY_GFX_BMP+0x12c0, , MENU_BANK_SHOP, { shopOutGfxDay } )
-#pragma region( bankShopOutG2, NIGHT_GFX_BMP, NIGHT_GFX_BMP+0x12c0, , MENU_BANK_SHOP, { shopOutGfxNight } )
+#pragma region( bankShopOutG2, NIGHT_GFX_BMP, 0xbc00, , MENU_BANK_SHOP, { shopOutGfxNight } )
 
 #pragma data ( shopOutGfxDay )
 __export const char shopOutGfx1[] = {
@@ -37,9 +51,29 @@ __export const char shopOutGfx2[] = {
     #embed 0x01e0 0x232a "assets/multicolorGfx/shopOutside_31.10.22.kla"
 };
 
+__export const char shopOutGfx3[] = {
+    #embed 0x0400 20 "assets/sprites/piesGora.spd"
+};
+// const data on CRT
+#pragma data ( shopOutData )
+static const char shopOutAnimDelays[SHOP_OUTSIDE_ANIM_FRAMES] = { 64, 8, 6, 3, 6, 8, 255, 8, 6, 3, 6, 8 };
+
 // menu code is in ROM - data in RAM
 #pragma code ( shopOutCode )
 #pragma data ( data )
+
+static void _menuSpriteLoader(){
+    char i = 0;
+    do {
+#assign y 0
+#repeat
+       ((volatile char*) MENU_SPRITE_DST)[y + i] = shopOutGfx3[y + i];
+#assign y y + 0x100
+#until y == 0x0400
+        i++;
+    } while (i != 0);
+#undef y
+}
 
 static bool _checkIfOpen(){
     // weekdays
@@ -52,6 +86,39 @@ static bool _checkIfOpen(){
             return true;
 
     return false;
+}
+
+
+__interrupt static void _shopOutsideShowSprites(){
+    vic.spr_enable   = 0b00000011;
+    vic.spr_expand_x = 0b00000000;
+    vic.spr_expand_y = 0b00000000;
+    vic.spr_priority = 0b00000000;
+    vic.spr_mcolor0  = SHOP_OUTSIDE_SMC1;
+    vic.spr_mcolor1  = SHOP_OUTSIDE_SMC2;
+    vic.spr_multi    = 0b11111111;
+    vic.spr_msbx     = 0b00000000;
+
+    vic.spr_pos[0].x = SHOP_OUTSIDE_ANIM_X;
+    vic.spr_pos[1].x = SHOP_OUTSIDE_ANIM_X;
+    vic.spr_pos[0].y = SHOP_OUTSIDE_ANIM_Y;
+    vic.spr_pos[1].y = SHOP_OUTSIDE_ANIM_Y+21;
+
+    vic.spr_color[0] = SHOP_OUTSIDE_ANIM_C1;
+    vic.spr_color[1] = SHOP_OUTSIDE_ANIM_C2;
+
+    char sprPtr = SHOP_OUTSIDE_SPR_BANK_ANIM + animFrame;
+    GFX_1_SCR[OFFSET_SPRITE_PTRS+0] = sprPtr;
+    GFX_1_SCR[OFFSET_SPRITE_PTRS+1] = SHOP_OUTSIDE_SPR_BANK;
+
+    animDelay--;
+    if(animDelay == 0){
+        animDelay = shopOutAnimDelays[animFrame];
+        
+        animFrame++;
+        if(animFrame == SHOP_OUTSIDE_ANIM_FRAMES)
+            animFrame = 0;
+    }
 }
 
 // Switching code generation back to shared section
@@ -80,13 +147,13 @@ static void _getInside(){
 #pragma code ( shopOutCode )
 #pragma data ( data )
 
-static void _soMenu2(){
-    updateStatusBar("Sklep menu, opcja 2");
-}
+// static void _soMenu2(){
+//     updateStatusBar("Sklep menu, opcja 2");
+// }
 
 const struct MenuOption SHOP_OUTSIDE_MENU[] = {
     { TXT_IDX_MENU_SHOPOUT1, '1', SCREEN_SPLIT_MC_TXT, UI_SELECT, &_getInside, 0, 1, 4},
-    { TXT_IDX_MENU_SHOPOUT2, '2', SCREEN_SPLIT_MC_TXT, UI_SELECT, &_soMenu2, 0, 1, 5},
+    // { TXT_IDX_MENU_SHOPOUT2, '2', SCREEN_SPLIT_MC_TXT, UI_SELECT, &_soMenu2, 0, 1, 5},
     { TXT_IDX_EXIT_TO_MAP, KEY_ARROW_LEFT, SCREEN_TRANSITION, UI_LF+UI_SELECT, &showMenu, MENU_BANK_MAP_VILLIAGE_1, 1, 8},
     // { TXT_IDX_MENU_EXIT, KEY_ARROW_LEFT, SCREEN_SPLIT_MC_TXT, UI_LF, &showMenu, MENU_BANK_MAIN_MENU, 2, 7},
     END_MENU_CHOICES
@@ -94,7 +161,7 @@ const struct MenuOption SHOP_OUTSIDE_MENU[] = {
 
 static void _menuHandler(void){
     loadMenuGfx();
-
+    loadMenuSprites();
     // Prepare output window
     cwin_init(&cw, GFX_1_SCR, SCREEN_X_START, SCREEN_Y_START, SCREEN_WIDTH, SCREEN_HEIGHT);
     cwin_clear(&cw);
@@ -110,9 +177,9 @@ static void _menuHandler(void){
 __export static const Loaders menuLoaders = {
     .loadMenuCode    = &menuNoop,
     .loadMenuGfx     = &menuGfxLoader,
-    .loadMenuSprites = &menuSpriteLoader,
+    .loadMenuSprites = &_menuSpriteLoader,
     .showMenu        = &_menuHandler,
-    .showSprites     = &spriteNoop,
+    .showSprites     = &_shopOutsideShowSprites,
     .updateMenu      = &menuNoop,
     .runMenuLoop     = &menuNoop,
 };
